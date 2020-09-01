036c6809357168d9df0344cb5cd5053a62873581
Adam Smyczek <adamsmyczek@gmail.com>
2020-09-01 18:55:13 +0200

    Bug fixes for first integer testing.

--- service/schedule_controller.py
+++ service/schedule_controller.py
@@ -4,16 +4,17 @@ from threading import Thread, Event
 from dataclasses import dataclass
 from typing import Callable
 from datetime import datetime, timedelta
-from queue import PriorityQueue
 from salsa import salsa
 from time import sleep
+from json import dumps
+from service.utils import PeekPriorityQueue
+import paho.mqtt.client as mqtt
 import logging
 
 
 SLEEP_INTERVAL = 2  # in seconds
 PULL_INTERVAL = 5   # in minutes
 
-LOAD_SHEDDING_STAGE = 'LOAD_SHEDDING_STAGE'
 ALERT_LOAD_SHEDDING_START = 'LOAD_SHEDDING_START'
 ALERT_LOAD_SHEDDING_END = 'LOAD_SHEDDING_END'
 ALERT_POWER_OUTAGE_START = 'POWER_OUTAGE_START'
@@ -36,35 +37,45 @@ class ScheduleEvent:
 
 def future_event(event_time):
     diff = event_time.replace(second=0, microsecond=0) - \
-            datetime.now(tz=None).replace(second=0, microsecond=0)
-    return diff and (1, -1)[diff < 0]
+            datetime.now().replace(second=0, microsecond=0).astimezone(tz=None)
+    return diff.total_seconds()
 
 
-def alert_event(alert, time):
-    def builder_function(mqtt_client, config):
+def alert_event(mqtt_client, config):
+    def builder_function(alert, time):
         def event_function():
-            logging.debug(f'Sending alert {alert}.')
+            logging.debug(f'Publishing /alert {alert}')
             mqtt_client.publish(f'{config("mqtt", "topic")}/alert', alert, qos=2, retain=False)
         return ScheduleEvent(time, event_function)
     return builder_function
 
 
+def on_syc(controller):
+    def on_message(client, user_data, message):
+        logging.debug('Forcing status update.')
+        stage = controller.query_stage(force=True)
+        controller.set_stage(stage)
+        logging.debug(f'Updated load shedding stage to {stage}')
+    return on_message
+
+
 class ScheduleController(Thread):
 
-    def __init__(self, config, mqtt_client):
+    def __init__(self, config, mqtt_client: mqtt.Client):
         Thread.__init__(self, name='Schedule controller')
         self._mqtt_client = mqtt_client
+        self._mqtt_client.on_message = on_syc(self)
         self._config = config
         self._stopper = Event()
-        self._event_queue = PriorityQueue()
+        self._event_queue = PeekPriorityQueue()
         self._stage = 0
-        self._query_stage()
+        self.query_stage()
 
     def stop(self):
         if not self._stopper.is_set():
             self._stopper.set()
-        self.join()
         self._clear_queue()
+        self.join()
         logging.info('Schedule controller stopped.')
 
     def _schedule_event(self, event: ScheduleEvent):
@@ -75,15 +86,22 @@ class ScheduleController(Thread):
 
     def _clear_queue(self):
         while not self._event_queue.empty():
-            self._event_queue.empty()
+            self._event_queue.get()
 
-    def _set_stage(self, stage):
+    def set_stage(self, stage):
         self._stage = stage
         self._clear_queue()
-        now = datetime.now(tz=None)
+        now = datetime.now().astimezone(tz=None)
         if stage > 0:
             logging.debug('Creating alerts...')
-            for s in salsa.get_schedule(stage, block=self._config('salsa', 'block'), days=2)['schedule']:
+            schedule = salsa.get_schedule(stage, block=self._config('salsa', 'block'), days=2)
+            pub_schedule = dumps({'stage': stage,
+                                  'block': schedule['block'],
+                                  'schedule': [{'start': s['start'].isoformat(), 'end': s['end'].isoformat()}
+                                               for s in schedule['schedule']]})
+            logging.debug(f'Publishing /schedule {pub_schedule}')
+            self._mqtt_client.publish(f'{self._config("mqtt", "topic")}/schedule', pub_schedule, qos=2, retain=False)
+            for s in schedule['schedule']:
                 start = s['start']
                 if start > now:
                     alert_builder = alert_event(self._mqtt_client, self._config)
@@ -94,39 +112,39 @@ class ScheduleController(Thread):
                     self._schedule_event(alert_builder(ALERT_ROWER_OUTAGE_30MIN, start - timedelta(minutes=-30)))
                     self._schedule_event(alert_builder(ALERT_ROWER_OUTAGE_END, s['end']))
 
-    def _query_stage(self):
+    def query_stage(self):
         logging.debug('Requesting load shedding stage.')
         if (new_stage := salsa.get_stage()) >= 0:
+            logging.debug(f'Publishing /stage {new_stage}')
             self._mqtt_client.publish(f'{self._config("mqtt", "topic")}/stage', new_stage, qos=2, retain=False)
             if new_stage != self._stage:
-                logging.debug(f'Load shedding stage changed to {new_stage}.')
-                self._mqtt_client.publish(f'{self._config("mqtt", "topic")}/alert',
-                                          ALERT_LOAD_SHEDDING_START if new_stage > 0 else ALERT_LOAD_SHEDDING_END,
-                                          qos=2,
-                                          retain=False)
-                self._set_stage(new_stage)
+                alert = ALERT_LOAD_SHEDDING_START if new_stage > 0 else ALERT_LOAD_SHEDDING_END
+                logging.debug(f'Publishing /alert {alert}')
+                self._mqtt_client.publish(f'{self._config("mqtt", "topic")}/alert', alert, qos=2, retain=False)
+                self.set_stage(new_stage)
         else:
             logging.error(f'Load shedding query returned with error code {new_stage}')
+        return new_stage
 
     def run(self):
         logging.info('Starting scheduler controller.')
         interval = self._config('salsa', 'interval') or PULL_INTERVAL
         while not self._stopper.is_set():
-            now = datetime.now(tz=None)
+            now = datetime.now().astimezone(tz=None)
             if now.second < SLEEP_INTERVAL and (now.minute % interval) == 0:
 
                 # ping query status
-                self._query_stage()
+                self.query_stage()
 
                 # remove previous dates
                 while not self._event_queue.empty() and \
-                        future_event(self._event_queue[0].key) < 0:
+                        future_event(self._event_queue.peek().key) < 0:
                     event = self._event_queue.get()
                     logging.warning(f'Past event at {event.key} identified. Discarding.')
 
                 # process current event
                 while not self._event_queue.empty() and \
-                        future_event(self._event_queue[0].key) == 0:
+                        future_event(self._event_queue.peek().key) == 0:
                     self._event_queue.get().value()
 
             sleep(SLEEP_INTERVAL)
